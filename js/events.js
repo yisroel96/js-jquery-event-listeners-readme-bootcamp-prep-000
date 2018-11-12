004d938ae34932a9ff120dcc1020122668ab7ea6
witty-collection-3981 <yisroel96@gmail.com>
2018-11-12 19:36:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,10 +20,11 @@ function pressIt() {
   })
 }
 
-
-
-
-
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
